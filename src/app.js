5bf49d620ddfd959250620f53e8c5fa3305bab34
Nsuliman <nsuliman88@gmail.com>
2020-03-02 17:08:53 +0200

    coding completed and works

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from './moduls/Header.js';
-import Footer from './moduls/Footer.js';
-import Counter from './moduls/Counter.js';
+import Header from './modules/Header.js';
+import Footer from './modules/Footer.js';
+import Counter from './modules/Counter.js';
 
 import './app.css';
 
