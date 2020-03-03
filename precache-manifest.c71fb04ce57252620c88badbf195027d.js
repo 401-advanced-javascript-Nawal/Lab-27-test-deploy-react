93b5a4378180022dd0c0667ea124e36f57775585
Nsuliman <runner@fv-az78.fjfvhpejuhleblmkl00a51gyee.cx.internal.cloudapp.net>
2020-03-03 18:58:27 +0000

    Updates

---
+++ precache-manifest.c71fb04ce57252620c88badbf195027d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d8151b3bae001e25ff249af379a0867",
+    "url": "/Lab-27-test-deploy-react/index.html"
+  },
+  {
+    "revision": "1ab430148e9fdfed532d",
+    "url": "/Lab-27-test-deploy-react/static/css/main.cadfcf34.chunk.css"
+  },
+  {
+    "revision": "6998453f0229c1371752",
+    "url": "/Lab-27-test-deploy-react/static/js/2.412bd30c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Lab-27-test-deploy-react/static/js/2.412bd30c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ab430148e9fdfed532d",
+    "url": "/Lab-27-test-deploy-react/static/js/main.36103555.chunk.js"
+  },
+  {
+    "revision": "cb296a1fedde62bd20ea",
+    "url": "/Lab-27-test-deploy-react/static/js/runtime-main.34a248a2.js"
+  }
+]);
