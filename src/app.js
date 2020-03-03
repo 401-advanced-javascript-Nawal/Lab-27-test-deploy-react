33c8c20a9d7a2b3eab2dbe4c3a6f58b7a7d06d13
Nsuliman <nsuliman88@gmail.com>
2020-03-03 21:28:52 +0200

    Netlify : https://objective-franklin-577a0a.netlify.com/

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import Header from './modules/Header.js';
 import Footer from './modules/Footer.js';
 import Counter from './modules/Counter.js';
 
+// Netlify Deployment : https://objective-franklin-577a0a.netlify.com/
+
 import './app.scss';
 
 class App extends React.Component {
